569b28622b1b03c8ad2e812fc94cd059de8eeb8f
guyman <guy.manzurola@gmail.com>
2020-09-28 13:50:08 +0300

    initial commit

---
+++ precache-manifest.0f4cb731b20a7697c1d739c5a8eb41b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f48f33f17c5a52dc61394b0f5f6605",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f7fc3e0ad1a99a520eb",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "2ce92394c0adb4802280",
+    "url": "/static/css/main.3b358fb9.chunk.css"
+  },
+  {
+    "revision": "9f7fc3e0ad1a99a520eb",
+    "url": "/static/js/2.6bd5e577.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6bd5e577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ce92394c0adb4802280",
+    "url": "/static/js/main.014af9f7.chunk.js"
+  },
+  {
+    "revision": "01b1bf82a33c87a5ee27",
+    "url": "/static/js/runtime-main.63044044.js"
+  },
+  {
+    "revision": "6e8e971b0ab89ebd956fd9fbf10c55fe",
+    "url": "/static/media/good.6e8e971b.png"
+  },
+  {
+    "revision": "d717b055536881f310f8f49d21b7df33",
+    "url": "/static/media/noun_Check Mark_1747191.d717b055.png"
+  },
+  {
+    "revision": "3bb6edc05d3613fb0fbf40759fac3929",
+    "url": "/static/media/noun_like_997801.3bb6edc0.png"
+  }
+]);
