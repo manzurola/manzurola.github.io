1c8fcebd4c625039afe6b94f98b7ca7c8dd35278
guyman <guy.manzurola@gmail.com>
2020-09-29 10:36:26 +0300

    Add files via upload

---
+++ precache-manifest.0cd8bce3fb80e4024b159d1edfec7a6b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d53d09feaebc2a94e59c56f686d761",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5b793218374cecc16c5",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "5095976889dfe6ecc9d5",
+    "url": "/static/css/main.3b358fb9.chunk.css"
+  },
+  {
+    "revision": "e5b793218374cecc16c5",
+    "url": "/static/js/2.c349bf10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c349bf10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5095976889dfe6ecc9d5",
+    "url": "/static/js/main.4749c1a3.chunk.js"
+  },
+  {
+    "revision": "01b1bf82a33c87a5ee27",
+    "url": "/static/js/runtime-main.63044044.js"
+  },
+  {
+    "revision": "6e8e971b0ab89ebd956fd9fbf10c55fe",
+    "url": "/static/media/good.6e8e971b.png"
+  },
+  {
+    "revision": "d717b055536881f310f8f49d21b7df33",
+    "url": "/static/media/noun_Check Mark_1747191.d717b055.png"
+  },
+  {
+    "revision": "3bb6edc05d3613fb0fbf40759fac3929",
+    "url": "/static/media/noun_like_997801.3bb6edc0.png"
+  }
+]);
